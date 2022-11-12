b57d46d45faa1a95ade3aa5461ca1dd41b988353
Renata Smolska <sana22_srs@student.ztu.edu.ua>
2022-11-12 02:20:41 +0200

    Changed 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana");
+Console.WriteLine("Commerce Software");
+Console.WriteLine("Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
